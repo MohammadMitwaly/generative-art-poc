76ea5b4081d66b8026e7032f808ca8efb80d86eb
Mo. Mitwaly <mhmadmtwale@gmail.com>
2021-04-15 19:05:47 +0300

    Made breaking changes, not working currently

--- src/components/ImageSketch.tsx
+++ src/components/ImageSketch.tsx
@@ -8,6 +8,7 @@ interface ImageSketchProps {
 
 const ImageSketch: React.FC<ImageSketchProps> = (props: ImageSketchProps) => {
   let img: p5Types.Image;
+  let canvas;
   const preload = (p5: p5Types) => {
     img = props.imageLocalURL
       ? p5.loadImage(props.imageLocalURL)
@@ -15,25 +16,28 @@ const ImageSketch: React.FC<ImageSketchProps> = (props: ImageSketchProps) => {
   };
 
   const setup = (p5: p5Types, canvasParentRef: Element) => {
-    p5.createCanvas(500, 500).parent(canvasParentRef);
-    img && p5.image(img, 0, 0);
+    if (img) {
+      canvas = p5.createCanvas(img.width, img.height);
+      let canvasX = (p5.windowWidth - img.width) / 2;
+      let canvasY = (p5.windowHeight - img.height) / 2;
+      canvas.position(canvasX, canvasY);
+
+      for (let column = 0; column < img.width; column += 1) {
+        for (let row = 0; row < img.height; row += 1) {
+          let pixel = img.get(column, row);
+          // let r = p5.random(255); // r is a random number between 0 - 255
+          // let g = p5.random(100, 200); // g is a random number betwen 100 - 200
+          // let b = p5.random(100); // b is a random number between 0 - 100
+          // let a = p5.random(200, 255); // a is a random number between 200 - 255
+          p5.stroke(p5.color(pixel));
+          p5.strokeWeight(1);
+          p5.point(column, row);
+        }
+      }
+    }
   };
 
   const draw = (p5: p5Types) => {
-    let canvasX = (p5.windowWidth - img.width) / 2;
-    let canvasY = (p5.windowHeight - img.height) / 2;
-    for (let column = 0; column < img.width; column += 8) {
-      for (let row = 0; row < img.height; row += 8) {
-        let pixel = img.get(column, row);
-        let r = p5.random(255); // r is a random number between 0 - 255
-        let g = p5.random(100, 200); // g is a random number betwen 100 - 200
-        let b = p5.random(100); // b is a random number between 0 - 100
-        let a = p5.random(200, 255); // a is a random number between 200 - 255
-        p5.stroke(r, g, b, a);
-        p5.strokeWeight(12);
-        p5.point(column, row);
-      }
-    }
     p5.noLoop();
   };
 
