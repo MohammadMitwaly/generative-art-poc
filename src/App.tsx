f500ed995fdbd04b8ff2edc10d3c556b19ec3418
Mo. Mitwaly <mhmadmtwale@gmail.com>
2021-04-21 11:36:13 +0300

    Add router boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import ImageSketch from "./components/ImageSketch";
 import { Grid, withStyles } from "@material-ui/core";
 import ImageUploader from "react-images-upload";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const styles = {
   input: {
@@ -20,23 +21,31 @@ const App = () => {
   // TODO: Show image and remove the load-file component
 
   return (
-    <div className="App">
-      <Grid container style={{ minHeight: "15vh" }}>
-        <Grid item style={{ margin: "10px", width: "100%" }}>
-          <ImageUploader
-            withIcon={true}
-            buttonText="Choose an image"
-            onChange={onDrop}
-            imgExtension={[".jpg", ".gif", ".png", ".gif"]}
-            maxFileSize={5242880}
-          />
+    <Router>
+      <div className="App">
+        <Grid container style={{ minHeight: "15vh" }}>
+          <Switch>
+            <Route path="/sketch">
+              <Grid item style={{ margin: "10px", width: "100%" }}>
+                {url && <ImageSketch imageLocalURL={url} />}
+              </Grid>
+            </Route>
+            <Route path="/">
+              <Grid item style={{ margin: "10px", width: "100%" }}>
+                <ImageUploader
+                  withIcon={true}
+                  buttonText="Choose an image"
+                  onChange={onDrop}
+                  imgExtension={[".jpg", ".gif", ".png", ".gif"]}
+                  maxFileSize={5242880}
+                />
+              </Grid>
+            </Route>
+          </Switch>
         </Grid>
-        <Grid item style={{ margin: "10px", width: "100%" }}>
-          {url && <ImageSketch imageLocalURL={url} />}
-        </Grid>
-      </Grid>
-      <br />
-    </div>
+        <br />
+      </div>
+    </Router>
   );
 };
 
