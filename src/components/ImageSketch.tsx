bad24bef808c649858acd3e328806caa192e8839
Mo. Mitwaly <mhmadmtwale@gmail.com>
2021-06-19 18:44:28 +0300

    Add ability to save the sketch automatically

--- src/components/ImageSketch.tsx
+++ src/components/ImageSketch.tsx
@@ -2,6 +2,7 @@ import React, { Dispatch, SetStateAction, useState } from "react";
 import Sketch from "react-p5";
 import p5Types from "p5";
 import { Redirect, useHistory } from "react-router";
+import "../App.css";
 
 interface ImageSketchProps {
   imageLocalURL: string;
@@ -10,6 +11,8 @@ interface ImageSketchProps {
 
 const ImageSketch: React.FC<ImageSketchProps> = (props: ImageSketchProps) => {
   const history = useHistory();
+  const [image, setImage] = useState<undefined | p5Types.Renderer>(undefined);
+  const [p5Instance, setP5Instance] = useState<undefined | p5Types>();
   let img: p5Types.Image;
   let canvas;
   const [imageUrl, setImageUrl] = useState(props.imageLocalURL);
@@ -49,6 +52,8 @@ const ImageSketch: React.FC<ImageSketchProps> = (props: ImageSketchProps) => {
           p5.pop();
         }
       }
+      setImage(canvas);
+      setP5Instance(p5);
     }
   };
 
@@ -64,6 +69,7 @@ const ImageSketch: React.FC<ImageSketchProps> = (props: ImageSketchProps) => {
         style={{
           position: "absolute",
           top: "20px",
+          border: "none",
           height: "30px",
           borderRadius: "8%",
           backgroundColor: "rgb(242, 235, 235)",
@@ -73,6 +79,24 @@ const ImageSketch: React.FC<ImageSketchProps> = (props: ImageSketchProps) => {
         }}
       >
         Reset Sketch
+      </button>
+      <button
+        style={{
+          position: "absolute",
+          top: "55px",
+          height: "30px",
+          borderRadius: "8%",
+          border: "none",
+          backgroundColor: "rgb(242, 235, 235)",
+          color: "hsl(0, 1%, 18%)",
+          fontWeight: "bold",
+          fontFamily: "Roboto",
+        }}
+        onClick={() => {
+          p5Instance && p5Instance.save(image, "generated.png");
+        }}
+      >
+        Save image
       </button>
       <Sketch setup={setup} draw={draw} preload={preload} />
     </div>
